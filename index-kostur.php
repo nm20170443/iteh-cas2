2f0d566e994611abfc1a513588c9086d61092163
Nemanja Markovic <necamark@gmail.com>
2022-10-13 15:31:34 +0200

    Promenjen index.php

--- index-kostur.php
+++ index-kostur.php
@@ -22,7 +22,7 @@
         <div class="main-div">
             <form method="POST" action="#">
                 <div class="container">
-                    <label class="username">Korisnicko ime</label>
+                    <label class="username">Admin username</label>
                     <input type="text" name="username" class="form-control"  required>
                     <br>
                     <label for="password">Lozinka</label>
